3af1d1c225b8ea6ef2dfed5c2fc920b9a2423f16
Pedro Borba <pedro_borba77@hotmail.com>
2021-06-30 01:39:25 -0300

    corrigindo problemas no resposivo.

--- src/js/slides.js
+++ src/js/slides.js
@@ -15,16 +15,29 @@ var swiper = new Swiper('.swiper-container-1', {
   },
 });
 
-var swiper2 = new Swiper('.swiper-container-2', {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  loop: true,
-  pagination: {
-    el: '.swiper-pagination-2',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next-2',
-    prevEl: '.swiper-button-prev-2',
-  },
-});
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
